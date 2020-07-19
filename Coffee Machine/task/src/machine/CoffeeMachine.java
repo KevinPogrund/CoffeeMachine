9022276f6e6909c8c164c015c7a076eae5f94c01
KevinPogrund <kpogrund@gmail.com>
2020-07-19 21:30:11 +0200

    Update CoffeeMachine.java
    The changes to the source file for Stage 6: Brush up your code

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,16 +1,20 @@
 package machine;
+
 import java.util.*;
 
 public class CoffeeMachine {
-    protected static final Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
     //                       water, milk beans cups, $
-    private static int[] machine = new int[]{400, 540, 120, 9, 550};
-    private static int[] espresso = new int[]{250, 0, 16, 4};
-    private static int[] latte = new int[]{350, 75, 20, 7};
-    private static int[] cappo = new int[]{200, 100, 12, 6};
-    private static int[][] buying = new int[][]{machine, espresso, latte, cappo};
+    //final static int[] machine = new int[]{400, 540, 120, 9, 550};
+    final static int[] espresso = new int[]{250, 0, 16, 4};
+    final static int[] latte = new int[]{350, 75, 20, 7};
+    final static int[] cappo = new int[]{200, 100, 12, 6};
+    final static int[][] drink= new int[][]{espresso, latte, cappo};
+    static Machine mach = new Machine();
 
     public static void main(String[] args) {
+
+        // Scanner sc = new Scanner(System.in);
         ask();
     }
 
@@ -26,7 +30,7 @@ public class CoffeeMachine {
         } else if (does.equals("take")) {
             takeit();
         } else if (does.equals("remaining")) {
-            System.out.println(outp(buying[0]));
+            System.out.println(mach.toString());
             ask();
         } else {
             System.exit(0);
@@ -34,8 +38,8 @@ public class CoffeeMachine {
     }
 
     private static void takeit() {
-        System.out.println("I gave you $" + buying[0][4]);
-        buying[0][4] = 0;
+        System.out.println("I gave you $" + mach.getMoney());
+        mach.setMoney(0);
         ask();
     }
 
@@ -49,9 +53,7 @@ public class CoffeeMachine {
         fill[2] = sc.nextInt();
         System.out.println("Write how many disposable cups of coffee do you want to add:\n");
         fill[3] = sc.nextInt();
-        for (int i = 0; i < fill.length; i++) {
-            buying[0][i] += fill[i];
-        }
+        mach.fillerup(fill);
         ask();
     }
 
@@ -59,19 +61,15 @@ public class CoffeeMachine {
         try {
             System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
             int bought = sc.nextInt();
-            String good = enough(bought);
+            String good = mach.enough(drink[bought-1]);
             if (good.equals("yes")) {
                 System.out.println("I have enough resources, making you a coffee!");
-                for (int i = 0; i < 3; i++) {
-                    buying[0][i] -= buying[bought][i];
-                }
-                buying[0][3] -= 1;
-                buying[0][4] += buying[bought][3];
+                mach.purchase(drink[bought-1]);
             } else {
                 System.out.println("Sorry, not enough " + good + "!\n");
             }
         } catch (Exception e) {
-            System.out.println(outp(buying[0]));
+            System.out.println(mach.toString());
         }
         ask();
     }
@@ -83,16 +81,5 @@ public class CoffeeMachine {
                 m[2] + " of coffee beans\n" +
                 m[3] + " of disposable cups\n" +
                 m[4] + " of money\n";
-    }
-
-    public static String enough(int choice) {
-        String[] product = new String[]{"water", "milk", "coffee"};
-        boolean good = true;
-        for (int i = 0; i < 3; i++) {
-            if (buying[0][i] < buying[choice][i]) {
-                return product[i];
-            }
-        }
-        return "yes";
     }
 }
